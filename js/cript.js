d3e1ec9abb73da08bf852280ee7655e12ebcb529
jonatan mansilla <yoniii-11@live.com.ar>
2022-07-25 18:22:08 -0300

    mi proyecto

--- js/cript.js
+++ js/cript.js
@@ -1,39 +1,79 @@
-//mi segunda pagina 
 
-
-var ingredientes = [];
-var cantidad = parseInt(prompt("¿Cuántos ingredientes vas a añadir?"));
-var contador = 1;
-
-while (contador <= cantidad) {
-  var ingrediente = prompt("Informe el ingrediente " + contador);
-  ingredientes.push(ingrediente);
-  contador++;
+function imprSelec(nombre) {
+  var ficha = document.getElementById(nombre);
+  var ventimp = window.open(' ', 'popimpr');
+  ventimp.document.write(ficha.innerHTML);
+  ventimp.document.close();
+  ventimp.print();
+  ventimp.close();
 }
 
-console.log(ingredientes);
 
 
-var ingredientes = [];
-var cantidad = parseInt(prompt("¿Cuántos ingredientes vas a añadir?"));
-var contador = 1;
+// CONEXION CON EL BOTON
+var botonAdicionar = document.querySelector("#miBoton");
+//forma anonima
 
-while (contador <= cantidad) {
-  var ingrediente = prompt("Informe el ingrediente " + contador);
-  var existe = false;
-  for (var posicion = 0; posicion < ingredientes.length; posicion++) {
-    if (ingredientes[posicion] == ingrediente) {
-      existe = true;
-      alert("repetido " + ingrediente);
-      break;
-    }
+
+botonAdicionar.addEventListener("click", function (event) {
+  event.preventDefault();
+  let form = document.querySelector("#formulariosCargar");
+  let nombre = form.nombre.value;
+  let altura = form.producto.value;
+  let peso = form.cantidades.value;
+  let local = form.local.value;
+
+  let pacienteTr = document.createElement("tr");
+  let nombreTd = document.createElement("td");
+  let alturaTd = document.createElement("td");
+  let pesoTd = document.createElement("td");
+  let gorduraTd = document.createElement("td");
+  let imcTd = document.createElement("td");
+
+  function multiplica(x, j) {
+    let re = x * j;
+    return re;
+
   }
-  if (existe == false) {
-    ingredientes.push(ingrediente);
-    contador++;
-  }
+  nombreTd.textContent = nombre;
+  alturaTd.textContent = altura;
+  pesoTd.textContent = peso;
+  gorduraTd.textContent = local;
+  imcTd.textContent = multiplica(altura, peso);
+
+  pacienteTr.appendChild(nombreTd);
+  pacienteTr.appendChild(alturaTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(gorduraTd);
+  pacienteTr.appendChild(imcTd);
+
+  var tabla = document.querySelector("#tabla-pacientes");
+
+  tabla.appendChild(pacienteTr);
+
+});
+
+
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var z = 0; z < pacientes.length; z++) {
+  var paciente = pacientes[z];
+  var tdPeso = paciente.querySelector(".info-peso");
+  var peso = tdPeso.textContent;
+
+  var tdAltura = paciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
+
+  var tdIMC = paciente.querySelector(".info-imc");
+  var tdIMC = paciente.querySelector(".precio-total");
+  pesoEsValido = true;
+  alturaEsValida = true;
 }
-console.log(ingredientes);
+
+//usamos la funcion en forma generica si es de muchos usos  titulo.addEventListener("click", mostrar);function mostrar() { console.log("perro"); }
+console.log(pacientes);
+
 
 
 
--- js/js.js
+++ js/js.js
@@ -1,22 +1,24 @@
 
 let btn = document.querySelector('#btn');
-let con = ('<img src="../img/alura.PNG"  width="400" height="200"> ');
+let con = ('<img src="https://c.tenor.com/qg8K8VOmzJwAAAAj/party-popper-confetti.gif"  width="400" height="400"> <br> <h1>felicidades  es correcto :</h1>');
 
-//
-//let cancelar = document.getElementById('#cancelar');
-//cancelar.addEventListener('click', () => {
-//  location.reload();
-//})
+let mens = ('<img src="https://c.tenor.com/fKmjHZoUYc4AAAAC/no-nodding.gif"  width="400" height="400"> ');
 
 // memorizar las palabres funciones constantes de entrada y de salida   los datos se  reciven en forma de   un valor el cual si no se ingresa  salta errror
 btn.addEventListener('click', () => {
     var random = Math.round(Math.random(1) * 10);
     const nums = document.querySelector('.n');
     let vale = nums.value;
-    if (vale == random) {
-        let caja = document.querySelector('#c');
-        caja.innerHTML = con;
+    let caja = document.querySelector('#c');
+   if (vale == random ) {
+        caja.innerHTML = con + ('<h1 class="text-center" style="color:white;">' + random + '</h1>');
+    } else{
+        caja.innerHTML = mens + (' <h1 class="text-center" style="color:white;">'+random+' te quedan de intentos '+intentos+'</h1>');
+    }
+ 
 
-    } else { alert(vale); }
-
+})
+var cancelar = document.getElementById('cancelar');
+cancelar.addEventListener('click', _ => {
+    location.reload();
 })
